f1455e83b3cfc16db1a97c1eb5300caec856f95b
emotionography1 <emotionography@my-emo.com>
2025-08-15 23:05:10 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-
-// Kakao 전역 (없어도 무방, 오류 회피용)
 window.Kakao = window.Kakao || undefined;
-
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode><App /></React.StrictMode>
 );
 
