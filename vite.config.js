4b880a48129dce3153967823063315b4e220d8f7
emotionography1 <emotionography@my-emo.com>
2025-08-15 22:31:29 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/emotionage/",
   server: { port: 5173, host: true },
   build: { outDir: "dist" }
 });
